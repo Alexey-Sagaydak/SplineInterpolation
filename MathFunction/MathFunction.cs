fad5151738d7c524aaac751ad7f95cfd4dc0a85d
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2023-03-29 15:09:57 +0700

    Добавьте файлы проекта.

---
+++ MathFunction/MathFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFunction
+{
+    public class MathFunction
+    {
+        private readonly Func<float, float> function;
+
+        public MathFunction(Func<float, float> _function)
+        {
+            function = _function;
+        }
+
+        public float Calculate(float x)
+        {
+            return function(x);
+        }
+           
+        public float CalculateDerivative(float x, int power, float step)
+        {
+            if (power == 0) return Calculate(x);
+            return (CalculateDerivative(x + step, power - 1, step)
+                - CalculateDerivative(x - step, power - 1, step)) / (2.0f * step);
+        }
+    }
+}
---
+++ SplineInterpolation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathFunction;
+namespace SplineInterpolation
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            float Func(float x)
+            {
+                return (float)Math.Pow(x, 2) + 2 * (float)Math.Log(x);
+            }
+
+            MathFunction.MathFunction mathFunction = new MathFunction.MathFunction(Func);
+            Console.WriteLine(mathFunction.CalculateDerivative(5, 5, 0.1f));
+        }
+    }
+}
